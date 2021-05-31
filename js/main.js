93ceede0fa712d595187c46061736da78b392cd6
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-05-31 12:00:38 +0300

    Условия если негативное, если больше и если равно

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-function generateGeoCoordinates(minNumber, maxNumber, afterDotNumber) {
-  let generateNumber = Math.random() * (maxNumber - minNumber) + minNumber;
-  generateNumber = generateNumber.toFixed(afterDotNumber);
-  return generateNumber;
+function generateGeoCoordinates(minNumber, maxNumber, afterDotNumber = 0) {
+  if (minNumber < 0) {
+    minNumber = Math.abs(minNumber);
+  }
+  if (maxNumber < 0) {
+    maxNumber = Math.abs(maxNumber);
+  }
+  if (minNumber > maxNumber) {
+    return;
+  }
+  if (minNumber === maxNumber) {
+    return maxNumber.toFixed(afterDotNumber);
+  }
+  const generateNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+  return generateNumber.toFixed(afterDotNumber);
 }
 
 generateGeoCoordinates(0, 5, 2);
