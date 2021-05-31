d1274adeecd2da13e4cdc27a107fc1e819c4194f
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-05-31 13:34:20 +0300

    Переименование переменной, перевод типа данных в число, генерация чисел

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 function generateGeoCoordinates(minNumber, maxNumber, afterDotNumber = 0) {
+  let generatedNumber;
+  minNumber = Number(minNumber);
+  maxNumber = Number(maxNumber);
+  afterDotNumber = Number(afterDotNumber);
   if (minNumber < 0) {
     minNumber = Math.abs(minNumber);
   }
   if (maxNumber < 0) {
     maxNumber = Math.abs(maxNumber);
   }
-  if (minNumber > maxNumber) {
-    return;
-  }
   if (minNumber === maxNumber) {
     return maxNumber.toFixed(afterDotNumber);
   }
-  const generateNumber = Math.random() * (maxNumber - minNumber) + minNumber;
-  return generateNumber.toFixed(afterDotNumber);
+  if (minNumber > maxNumber) {
+    generatedNumber = Math.random() * (minNumber - maxNumber) + maxNumber;
+  }
+  generatedNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+  return generatedNumber.toFixed(afterDotNumber);
 }
 
 generateGeoCoordinates(0, 5, 2);
