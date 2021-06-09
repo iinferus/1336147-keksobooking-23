1537ec9ef992a36c362fc317c263900452a74f67
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-06-09 14:41:40 +0300

    Правка

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const MIN_PRICE = 1;
+const MAX_PRICE = 1000000;
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 100;
+const MIN_GUESTS = 0;
+const MAX_GUESTS = 3;
+const CHECK_TIMES = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+const locationLat = {
+  min: 35.65000,
+  max: 35.70000,
+};
+const locationLng = {
+  min: 139.70000,
+  max: 139.80000,
+};
+
 function generateRandomNumber(minNumber, maxNumber, afterDotNumber = 0) {
   const lower = Math.min(Math.abs(minNumber), Math.abs(maxNumber));
   const upper = Math.max(Math.abs(minNumber), Math.abs(maxNumber));
@@ -11,65 +32,52 @@ function getRandomArrayElement(elements) {
   return elements[generateRandomNumber(0, elements.length - 1)];
 }
 
+function getRandomMultipleArrayElement(elements) {
+  const generateNumber = generateRandomNumber(1, elements.length);
+  let result = '';
+  for(let i = 0; i < generateNumber; i++) {
+    let multipleElement = elements[generateRandomNumber(0, elements.length - 1)];
+    if (result.includes(multipleElement)) {
+      multipleElement = elements[generateRandomNumber(0, elements.length - 1)];
+    }
+    result += `${multipleElement}${i === generateNumber - 1 ? '': ' '}`;
+  }
+  return result;
+}
+
+getRandomMultipleArrayElement(TYPES);
+
 let imgAdressPostfix = 0;
 
 const createAuthor = () => {
   imgAdressPostfix++;
-  if (imgAdressPostfix > 8) {
-    imgAdressPostfix = 1;
-  }
-  const postfix =
-    String(imgAdressPostfix).length === 1
-      ? `0${imgAdressPostfix}`
-      : imgAdressPostfix;
   return {
-    avatar: `img/avatars/user${postfix}.png`,
+    avatar: `img/avatars/user${imgAdressPostfix < 10? '0' : ''}${imgAdressPostfix}.png`,
   };
 };
 
 createAuthor();
 
-const MIN_PRICE = 1;
-const MAX_PRICE = 1000000;
-const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const MIN_ROOMS = 1;
-const MAX_ROOMS = 100;
-const MIN_GUESTS = 0;
-const MAX_GUESTS = 3;
-const CHEK_TIMES = ['12:00', '13:00', '14:00'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photos = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-
-const MIN_LAT = 35.65000;
-const MAX_LAT = 35.70000;
-const MIN_LNG = 139.70000;
-const MAX_LNG = 139.80000;
-
-function createLocation() {
+function getLocation() {
   return {
-    lat: generateRandomNumber(MIN_LAT, MAX_LAT, 5),
-    lng: generateRandomNumber(MIN_LNG, MAX_LNG, 5),
+    lat: generateRandomNumber(locationLat.min, locationLat.max, 5),
+    lng: generateRandomNumber(locationLng.min, locationLng.max, 5),
   };
 }
-
-const locationLat = createLocation().lat;
-const locationLng = createLocation().lng;
 
 function createOffer() {
   return {
     title: 'Потрясающее бунгало с выходом в море',
-    address: `${locationLat} ${locationLng}`,
+    addres: Object.values(getLocation()).join(', '),
     price: generateRandomNumber(MIN_PRICE, MAX_PRICE),
-    type: getRandomArrayElement(TYPE),
+    type: getRandomArrayElement(TYPES),
     rooms: generateRandomNumber(MIN_ROOMS, MAX_ROOMS),
     guests: generateRandomNumber(MIN_GUESTS, MAX_GUESTS),
-    checkin: getRandomArrayElement(CHEK_TIMES),
-    checkout: getRandomArrayElement(CHEK_TIMES),
-    features: getRandomArrayElement(FEATURES),
+    checkin: getRandomArrayElement(CHECK_TIMES),
+    checkout: getRandomArrayElement(CHECK_TIMES),
+    features: getRandomMultipleArrayElement(FEATURES),
     description: 'Собственный пляж на первой линии, потрясающий вид на море и закаты, вокруг лес и горы. Завтрак, обед и ужин шведский стол, все включено.',
-    photos: getRandomArrayElement(photos),
+    photos: getRandomMultipleArrayElement(PHOTOS),
   };
 }
 
