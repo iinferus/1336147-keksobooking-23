8f4e9df6c793e7cbd5d8506d8e69a07ee5c33043
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-06-22 17:51:37 +0300

    s

--- js/formStatus.js
+++ js/formStatus.js
@@ -1,22 +1,22 @@
 const adForm = document.querySelector('.ad-form');
-const adFormElement = adForm.querySelectorAll('.ad-form__element');
+const adFormElements = adForm.querySelectorAll('.ad-form__element');
 const adFormSubmitButton = adForm.querySelector('.ad-form__submit');
 const adFormUploadFieldset = adForm.querySelector('.ad-form-header');
 
 const filterMap = document.querySelector('.map__filters');
-const filterMapElement = filterMap.querySelectorAll('.map__filter');
+const filterMapElements = filterMap.querySelectorAll('.map__filter');
 const filterMapFeaturesFieldset = filterMap.querySelector('.map__features');
 
 function setInactiveState() {
   adForm.classList.add('ad-form--disabled');
-  for (const element of adFormElement) {
+  for (const element of adFormElements) {
     element.setAttribute('disabled', true);
   }
   adFormSubmitButton.setAttribute('disabled', true);
   adFormUploadFieldset.setAttribute('disabled', true);
 
   filterMap.classList.add('map__filters--disabled');
-  for (const element of filterMapElement) {
+  for (const element of filterMapElements) {
     element.setAttribute('disabled', true);
   }
   filterMapFeaturesFieldset.setAttribute('disabled', true);
@@ -24,14 +24,14 @@ function setInactiveState() {
 
 function setActiveState() {
   adForm.classList.remove('ad-form--disabled');
-  for (const fieldset of adFormElement) {
-    fieldset.removeAttribute('disabled');
+  for (const element of adFormElements) {
+    element.removeAttribute('disabled');
   }
   adFormSubmitButton.removeAttribute('disabled');
   adFormUploadFieldset.removeAttribute('disabled');
 
   filterMap.classList.remove('map__filters--disabled');
-  for (const element of filterMapElement) {
+  for (const element of filterMapElements) {
     element.removeAttribute('disabled');
   }
   filterMapFeaturesFieldset.removeAttribute('disabled');
