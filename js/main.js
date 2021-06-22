55a336e3e99807df58498dbaae5870e277b12e2c
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-06-22 09:53:56 +0300

    Блокировка и активация формы

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import {offersDataArrays} from './data.js';
 import {createSimilarAds} from './offer.js';
+import {setInactiveState, setActiveState} from './formStatus.js';
 const offerList = document.querySelector('#map-canvas');
 
+setInactiveState();
+setActiveState();
+
 offerList.appendChild(createSimilarAds(offersDataArrays));
+
