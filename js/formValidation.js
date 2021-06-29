46a92000bf774965c44cb0228a5b9b15b702e515
Herman Krasnopolskyi <herma.krasnopolskyi@gmail.com>
2021-06-29 15:57:12 +0300

    Правки

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,65 +1,42 @@
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-const MAX_PRICE = 1000000;
-
-const CAPACITY_GUESTS = {
-  1: [1],
-  2: [1, 2],
-  3: [1, 2, 3],
-  100: [0],
+const TYPES_MIN_PRICES = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
 };
 
 const adForm = document.querySelector('.ad-form');
-const adFormTitle = adForm.querySelector('#title');
 const adFormPrice = adForm.querySelector('#price');
 const adFormRoom = adForm.querySelector('#room_number');
 const adFormGuest = adForm.querySelector('#capacity');
+const adFormType = adForm.querySelector('#type');
 
-adFormTitle.addEventListener('input', () => {
-  const valueLength = adFormTitle.value.length;
+const typeKeys = Object.keys(TYPES_MIN_PRICES);
 
-  if (valueLength < MIN_TITLE_LENGTH) {
-    adFormTitle.setCustomValidity(`Минимальное количество символов - ${MIN_TITLE_LENGTH}. Осталось ввести ${MIN_TITLE_LENGTH - valueLength} символов`);
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    adFormTitle.setCustomValidity(`Максимальное количество символов - ${MAX_TITLE_LENGTH}. Удалите лишниее ${MIN_TITLE_LENGTH - valueLength} символов`);
-  } else {
-    adFormTitle.setCustomValidity('');
-  }
+adFormType.addEventListener('change', () => {
+  const valueType = adFormType.value;
 
-  adFormTitle.reportValidity();
-});
-
-adFormPrice.addEventListener('input', () => {
-  const minPrice = adFormPrice.min;
-  const valueLength = adFormPrice.value;
-  if (valueLength < minPrice) {
-    adFormPrice.setCustomValidity(`Минимальная цена - ${minPrice}.`);
-  } else if (valueLength > MAX_PRICE) {
-    adFormPrice.setCustomValidity(`Максимальная цена - ${MAX_PRICE}.`);
-  } else {
-    adFormPrice.setCustomValidity('');
-  }
-
-  adFormPrice.reportValidity();
-});
-
-const valueKeys = Object.keys(CAPACITY_GUESTS);
-
-adFormGuest.addEventListener('change', () => {
-  const valueRoom = adFormRoom.value;
-  const valueGuest = +adFormGuest.value;
-
-  valueKeys.forEach((key) => {
-    if(valueRoom === key) {
-      const capacityArray = CAPACITY_GUESTS[key];
-      const capacityArayIndex = capacityArray.indexOf(valueGuest);
-      if (capacityArayIndex === -1) {
-        adFormGuest.setCustomValidity('Не верное значение');
-      } else {
-        adFormGuest.setCustomValidity('');
-      }
+  typeKeys.forEach((typeKey) =>{
+    if (typeKey === valueType) {
+      const typeMinPrice = TYPES_MIN_PRICES[typeKey];
+      adFormPrice.placeholder = typeMinPrice;
+      adFormPrice.setAttribute('min', typeMinPrice);
     }
   });
+});
+
+adFormGuest.addEventListener('change', () => {
+  const valueRoom = +adFormRoom.value;
+  const valueGuest = +adFormGuest.value;
+
+  if (valueRoom === 100 && valueGuest > 0) {
+    adFormGuest.setCustomValidity('Для 100 комнат возможное значение 0 гостей');
+  } else if (valueRoom < valueGuest) {
+    adFormGuest.setCustomValidity('Гостей не может быть больше, чем комнат');
+  } else {
+    adFormGuest.setCustomValidity('');
+  }
 
   adFormGuest.reportValidity();
 });
